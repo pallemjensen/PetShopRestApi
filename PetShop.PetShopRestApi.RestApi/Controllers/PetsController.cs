b8e26baf45d444f20875e3f6a1c308b083e9da86
pallemjensen <31887698+pallemjensen@users.noreply.github.com>
2018-09-04 15:06:55 +0200

    With restapi

---
+++ PetShop.PetShopRestApi.RestApi/Controllers/PetsController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PetShop.Core.DomainService;
+using PetShop.Core.Entities;
+
+namespace PetShop.PetShopRestApi.RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PetsController : ControllerBase
+    {
+        private readonly IPetShopRepository _petShopRepository;
+
+        public PetsController(IPetShopRepository petShopRepository)
+        {
+            _petShopRepository = petShopRepository;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<Pet>> Get()
+        {
+           return _petShopRepository.ReadAllPets().ToList();
+        }
+    
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<Pet> Get(int id)
+        {
+            return _petShopRepository.GetPetById(id);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public ActionResult<Pet> Post([FromBody] Pet pet)
+        {
+            pet.PetId = 0;
+            return pet;
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] string value)
+        {
+
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            Pet pet = new Pet();
+            pet = _petShopRepository.GetPetById(id);
+
+            return Ok("Pet with id " + id + " was deleted.");
+        }
+    }
+}
---
+++ PetShop.PetShopRestApi.RestApi/Startup.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PetShop.Core.ApplicationService;
+using PetShop.Core.ApplicationService.Implementation;
+using PetShop.Core.DomainService;
+using PetShop.Infrastructure.Static.Data;
+using PetShop.Infrastructure.Static.Data.Repositories;
+
+namespace PetShop.PetShopRestApi.RestApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            FakeDb.InitData();
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddScoped<IPetShopRepository, PetShopRepository>();
+            services.AddScoped<IPetShopService, PetShopService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
